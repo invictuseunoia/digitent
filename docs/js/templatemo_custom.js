a46e2ba294700a0bb6dcc0aefaa8561c6380eae5
invictuseunoia <33128536+invictuseunoia@users.noreply.github.com>
2017-11-17 04:18:43 +0800

    Update templatemo_custom.js

--- docs/js/templatemo_custom.js
+++ docs/js/templatemo_custom.js
@@ -42,7 +42,7 @@ function initialize() {
 // load google map
 var script = document.createElement('script');
     script.type = 'text/javascript';
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBquDqt-wS2QjM5AT94mlEv001gONeeUvk&' +
+script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
         'callback=initialize';
     
     document.body.appendChild(script);
